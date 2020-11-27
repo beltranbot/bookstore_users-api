a78bc664c565d31137d529bb9dd99fe30e1e29e3
carlos.beltran <beltranbot@gmail.com>
2020-11-26 21:20:59 -0500

    initial project MVC structure

---
+++ controllers/users/users_controller.go
@@ -0,0 +1,22 @@
+package users
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// CreateUser func
+func CreateUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me!")
+}
+
+// GetUser func
+func GetUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me!")
+}
+
+// SearchUser func
+func SearchUser(c *gin.Context) {
+	c.String(http.StatusNotImplemented, "implement me!")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/beltranbot/bookstore_users-api/app"
+)
+
+func main() {
+	app.StartApplication()
+}
