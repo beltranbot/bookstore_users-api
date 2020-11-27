ffe050f3529f6de9463bd55149172774b54cfef6
carlos.beltran <beltranbot@gmail.com>
2020-11-26 22:54:17 -0500

    refactor to use common errors struct

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,14 +1,35 @@
 package users
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/beltranbot/bookstore_users-api/services"
+	"github.com/beltranbot/bookstore_users-api/utils/errors"
+
+	"github.com/beltranbot/bookstore_users-api/domain/users"
 
 	"github.com/gin-gonic/gin"
 )
 
 // CreateUser func
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	fmt.Println(user)
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		err := errors.NewBadRequestError("invalid json body")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	result, err := services.CreateUser(user)
+	if err != nil {
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 // GetUser func
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,15 @@
 package services
+
+import (
+	"net/http"
+
+	"github.com/beltranbot/bookstore_users-api/domain/users"
+	"github.com/beltranbot/bookstore_users-api/utils/errors"
+)
+
+// CreateUser func
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, &errors.RestErr{
+		Status: http.StatusInternalServerError,
+	}
+}
