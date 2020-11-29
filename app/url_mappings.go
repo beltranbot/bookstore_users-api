b01b141b73ad61b52538fac72222d54e33ee265b
carlos.beltran <beltranbot@gmail.com>
2020-11-28 19:32:03 -0500

    update rows

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -11,5 +11,6 @@ func mapUrls() {
 	router.POST("/users", users.CreateUser)
 	router.PUT("/users/:user_id", users.UpdateUser)
 	router.GET("/users/:user_id", users.GetUser)
+	router.PATCH("/users/:user_id", users.UpdateUser)
 	// router.GET("/users/search", controllers.SearchUser)
 }
